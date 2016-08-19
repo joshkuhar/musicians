3549e974bba24f0ac4654d08693d02a9871066fc
Josh Kuhar <jdkuhar@gmail.com>
2016-08-19 16:45:56 -0400

    update

--- musicians.js
+++ musicians.js
@@ -28,21 +28,6 @@ var Bassist = function() {
 };
 Bassist.prototype = Object.create(Musician.prototype);
 Bassist.prototype.constructor = Bassist;
-You have your basic objects here, but one more step is needed to allow you to require this file and use them. Node modules follow a specification called CommonJS. To follow the specification you need to put anything which you want to access through an imported module into an object called exports. So let's try it out with our three musician objects. Add the following code to the bottom of musicians.js:
-
-exports.Musician = Musician;
-exports.Guitarist = Guitarist;
-exports.Bassist = Bassist;
-Here you say that the three objects should be exported from the file. Now create a new file in the musicians directory called spinal_tap.js, and add the following code:
-
-var musicians = require('./musicians.js');
-
-var nigel = new musicians.Guitarist();
-nigel.tune();
-nigel.solo(8);
-
-var derek = new musicians.Bassist();
-derek.solo(4);
 
 exports.Musician = Musician;
 exports.Guitarist = Guitarist;
